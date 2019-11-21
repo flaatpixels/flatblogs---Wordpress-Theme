60e26d3875cc94507662d360b0b49cea9fdaa433
Flat-Pixels <pixelsplats@gmail.com>
2019-11-21 12:52:45 -0800

    feat: Add footer

--- footer.php
+++ footer.php
@@ -14,23 +14,23 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flatblogs' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'flatblogs' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'flatblogs' ), 'flatblogs', '<a href="http://underscores.me/">Steeve VERDIEU</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="site-container">
+			<section class="site-footer__about">
+				<h3 class="_header">Qui je suis ?</h3>
+				<p class="_about">
+					Je suis Développeur Web, pasionné des nouvelles technologies, 
+					amant du Digital Marketing, 
+					et surtout j’aime partager ce que j’apprends.
+				</p>
+				<div class="_social">
+					
+				</div>
+			</section>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<!-- <?php wp_footer(); ?> -->
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -65,5 +65,5 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
